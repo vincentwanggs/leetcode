b501ae8b9223fc458eef6eb298f71d8b2b19c4ce
vincentwanggs <vincentwanggs@users.noreply.github.com>
2014-09-11 18:13:03 +0800

    Update BinTreePostorderTraverse.java
    The accepted version

--- BinTreePostorderTraverse.java
+++ BinTreePostorderTraverse.java
@@ -8,32 +8,40 @@ public class BinTreePostorderTraverse{
       
       nodes.push(root);
       
-      TreeNode current = root;
+      TreeNode pre = null; // a pointer to indicate the previous visited node
       
+      // there are three cases when we do the postorder traversing
+      // 1) down the tree
+      // 2) traversing up from the left subtree
+      // 3) traversing up from the right subtree
+      
+      // to decide which direction we are now in, we need a pointer to record the previous node
       while (!nodes.isEmpty()){
-        if (current.left != null){
-          current = current.left;
-          nodes.push(current);
+        TreeNode n = nodes.peek();
+        if (pre == null || pre.left == n || pre.right == n){
+            // we are traversing down the tree
+            if (n.left != null)
+                nodes.add(n.left);
+            else if (n.right != null){
+                nodes.add(n.right);
+            }
+            else{
+                ret.add(nodes.pop().val);
+            }
         }
-        else if (current.right != null){
-          current = current.right;
-          nodes.push(current);
+        else if (pre == n.left){
+            // we are traversing up the tree from the left
+            if (n.right != null)
+                nodes.add(n.right);
+            else{
+                ret.add(nodes.pop().val);
+            }
         }
-        else{
-          current = nodes.pop();
-          ret.add(current.val);
-          if (nodes.isEmpty())
-            return ret;
-          if (nodes.peek().right == current){
+        else if (pre == n.right){
+            // we are traversing up the tree from the right tree
             ret.add(nodes.pop().val);
-            current = nodes.peek().right;
-          }
-          else{
-            current = nodes.peek().right;
-          }
-          if (current != null)
-            nodes.push(current);
         }
+        pre = n;
       }
       return ret;
       

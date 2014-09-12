d494051f1cad62f5a5263b329d3b171d37db5af7
vincentwanggs <vincentwanggs@users.noreply.github.com>
2014-09-12 10:49:40 +0800

    Update MaximumSubarray.java
    cur_max to store the current maximum sum. It is the larger of two: 1) the current element (if cur_max is negative) 2) the sum of cur_max and the current element (if both of them are positive)

--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -1,23 +1,22 @@
 
 // Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
 
+
 public class MaximumSubarray{
 
     public int maxSubArray(int[] A) {
       
-      int max = 0;
-      int cur_max = 0;
+      // note the condition that the subarray contains at least one element
+      int max = A[0];
+      int cur_max = A[0];
       
-      for (int i = 0; i < A.length; i ++){
-        if (A[i] >= 0){
-          if (cur_max+A[i] > max){
-            max = cur_max+A[i];
-            cur_max = max;
+      for (int i = 1; i < A.length; i ++){
+          cur_max = Math.max(cur_max+A[i], A[i]);
+          if (cur_max > max){
+            max = cur_max;
           }
-        }
-        else{
-          cur_max = 0;
-        }
+    
+       
       }
     }
 }

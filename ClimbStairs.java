605725aaa8e218925162ab544bec230e1f5ab5f4
vincentwanggs <vincentwanggs@users.noreply.github.com>
2014-09-12 10:17:09 +0800

    Create ClimbStairs.java

---
+++ ClimbStairs.java
@@ -0,0 +1,22 @@
+// store the previously computed result to save time
+// solved by dynamic programming
+
+public class ClimbStairs{
+
+    public int climbStairs(int n) {
+      
+      // initialisation of an array to store the number of ways to climb to level n stairs
+      
+      int[] steps = new int[n+1];
+      steps[1] = 1;
+      steps[2] = 2;
+      
+      for (int i = 3; i <= n; i ++){
+      // steps[i-1] is the number of steps if the last operation to level i is one step
+      // steps[i-2] is the number of steps if the last operation to level i is two steps
+        steps[i] = steps[i-1] + steps[i-2];
+      }
+      
+      return steps[n];
+    }
+}

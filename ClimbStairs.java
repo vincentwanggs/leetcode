17c9596231edf69ef314c30c501a7de442b1a742
vincentwanggs <vincentwanggs@users.noreply.github.com>
2014-09-12 10:19:06 +0800

    Update ClimbStairs.java
    The corner case where there are only two or less steps

--- ClimbStairs.java
+++ ClimbStairs.java
@@ -5,6 +5,8 @@ public class ClimbStairs{
 
     public int climbStairs(int n) {
       
+      if (n <= 2)
+            return n;
       // initialisation of an array to store the number of ways to climb to level n stairs
       
       int[] steps = new int[n+1];

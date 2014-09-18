688e19d912d9588fd1c864a9c1bcadc8772c6c54
vincentwanggs <vincentwanggs@users.noreply.github.com>
2014-09-18 11:40:19 +0800

    Update RotateImage.java

--- RotateImage.java
+++ RotateImage.java
@@ -2,7 +2,8 @@ public class RotateImage{
  public void rotate(int[][] matrix) {
         int n = matrix.length;
         for (int i = 0; i < n/2; i ++){
-			for (int j = 0; j < n/2; j ++){
+        	int last = n - 1 -i;
+			for (int j = i; j < last; j ++){
 				int upperLeft = matrix[i][j];
 				matrix[i][j] = matrix[n-1-j][i];
 				matrix[n-1-j][i] = matrix[n-1-i][n-1-j];

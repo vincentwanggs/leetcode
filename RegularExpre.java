30b8b3fd6b5723c6543ed54314501ab3a05d095c
vincentwanggs <vincentwanggs@users.noreply.github.com>
2014-10-08 13:26:11 +0800

    Create RegularExpre.java

---
+++ RegularExpre.java
@@ -0,0 +1,25 @@
+public class RegularExpre{
+  /*
+  '.' Matches any single character.
+  '*' Matches zero or more of the preceding element.
+  */
+  public boolean isMatch(String s, String p) {
+  
+    if (p.length() == 1 || p.charAt(1) != '*'){
+      if (s.length() < 1 || p.charAt(0) != '.' && s.charAt(0) != p.charAt(0))
+        return false;
+      return isMatch(s.substring(1),p.substring(1));
+    }
+    else{
+      int len = s.length();
+      int i = -1;
+      
+      while (i < len && (i < 0 || p.charAt(0) == '.' || p.charAt(0) == s.charAt(i))){
+        if (isMatch(s.substring(i+1), p.substring(2)))
+          return true;
+        i ++;
+      }
+      return false;
+    }
+  }
+}

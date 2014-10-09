6ac7185ac3ba058f9dc3c84736da8ef0c7919ef5
vincentwanggs <vincentwanggs@users.noreply.github.com>
2014-10-09 08:32:40 +0800

    Update WordBreakDP.java

--- WordBreakDP.java
+++ WordBreakDP.java
@@ -6,7 +6,7 @@ public class WordBreakDP{
     
     for (int i = 1; i <= s.length(); i ++){
       for (int j = i; j > 0; j --){ // for all the substrings before i
-        String substr = s.substring(j,i); // the substring from j to i
+        String substr = s.substring(j-1,i); // the substring from j to i
         if (dict.contains(substr) && canBreak[j-1]) // if s[0:j-1] can be broken, and s[j:i] is in the dictionary
           canBreak[i] = true;
       }

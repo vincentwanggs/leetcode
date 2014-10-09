60063443e2504cc2d81fa9f025ec3a3fde96a91c
vincentwanggs <vincentwanggs@users.noreply.github.com>
2014-10-09 17:09:18 +0800

    Update LargestRecHistogram.java

--- LargestRecHistogram.java
+++ LargestRecHistogram.java
@@ -4,7 +4,8 @@ public class LargestRecHistogram{
    // observation, for each hight, the left boundary is the leftmost height with greater or equal height
    // the right boundary is the rightmost height with greater or equal height
    int cur = 0, area = 0;
-   
+   // use a stack to keep all the indices of the increasing heights
+    Stack<Integer> left = new Stack<Integer>();  
       while (cur < height.length){
         if (left.empty() || height[cur] >= height[left.peek()]){
           left.push(cur);
@@ -12,7 +13,8 @@ public class LargestRecHistogram{
         }
         else{// we get a right boundary
           int top = left.pop();
-          // compute the area
+          // compute the area, the invarient is that all the rest elements in stack are lower than height[top]
+          // therefore, we can compute the area as height[top]*(left.isEmpty() ? cur : (cur-left.peek()-1))
           area = Math.max(area, height[top]*(left.isEmpty() ? cur : (cur-left.peek()-1)));  
         }
       }
